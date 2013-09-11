adbd24fc2a8fda33a82f96de52cf4ffe789fd1b3
Colm O Heigeartaigh <coheigea@apache.org>
2013-09-11 10:52:27 +0000

    Upgrading to CXF 2.7.7-SNAPSHOT  - Adding in FedizSAMLDelegationHandler
    git-svn-id: https://svn.apache.org/repos/asf/cxf/fediz/trunk@1521793 13f79535-47bb-0310-9956-ffa450edef68

---
+++ services/sts/src/main/java/org/apache/cxf/fediz/service/sts/FedizSAMLDelegationHandler.java
@@ -0,0 +1,53 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cxf.fediz.service.sts;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.cxf.sts.request.ReceivedToken;
+import org.apache.cxf.sts.token.delegation.TokenDelegationParameters;
+import org.apache.cxf.sts.token.delegation.TokenDelegationResponse;
+import org.apache.ws.security.saml.ext.AssertionWrapper;
+
+/**
+ * The SAML TokenDelegationHandler implementation. It disallows ActAs or OnBehalfOf for
+ * all cases apart from the case of a Bearer SAML Token. It differs from the SAMLDelegationHandler
+ * in the STS core, in that it doesn't require that the AppliesTo address match an 
+ * AudienceRestriction address in the token.
+ */
+public class FedizSAMLDelegationHandler 
+    extends org.apache.cxf.sts.token.delegation.SAMLDelegationHandler {
+    
+    @Override
+    public boolean canHandleToken(ReceivedToken delegateTarget) {
+        System.out.println("CANHANDLE");
+        return super.canHandleToken(delegateTarget);
+    }
+    @Override
+    public TokenDelegationResponse isDelegationAllowed(TokenDelegationParameters tokenParameters) { 
+        System.out.println("HERE!");
+        return super.isDelegationAllowed(tokenParameters);
+    }
+    @Override
+    protected List<String> getAudienceRestrictions(AssertionWrapper assertion) {
+        return Collections.emptyList();
+    }
+    
+}
