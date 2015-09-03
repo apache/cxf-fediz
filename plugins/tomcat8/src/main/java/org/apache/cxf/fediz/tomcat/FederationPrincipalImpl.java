0b2c2218bf313825a4bc03fbdf5207f431df22e5
Colm O hEigeartaigh <coheigea@apache.org>
2015-09-03 16:59:31 +0100

    [FEDIZ-124] - Fediz-plugin for Tomcat 8

---
+++ plugins/tomcat8/src/main/java/org/apache/cxf/fediz/tomcat/FederationPrincipalImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.fediz.tomcat;
+
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.apache.catalina.realm.GenericPrincipal;
+import org.apache.cxf.fediz.core.Claim;
+import org.apache.cxf.fediz.core.ClaimCollection;
+import org.apache.cxf.fediz.core.FederationPrincipal;
+
+@SuppressWarnings("deprecation")
+public class FederationPrincipalImpl extends GenericPrincipal implements FederationPrincipal {
+
+    protected ClaimCollection claims;
+    protected Element loginToken;
+
+    public FederationPrincipalImpl(String username, List<String> roles,
+            List<Claim> claims, Element loginToken) {
+        super(username, null, roles);
+        this.claims = new ClaimCollection(claims);
+        this.loginToken = loginToken;
+    }
+
+    public ClaimCollection getClaims() {
+        return this.claims;
+    }
+
+    @Override
+    public Element getLoginToken() {
+        return loginToken;
+    }
+
+}
