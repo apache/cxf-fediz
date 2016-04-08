b4410641cbcd8abd595a5fc69a8eb7bac5409fdd
Colm O hEigeartaigh <coheigea@apache.org>
2016-04-08 14:28:04 +0100

    Make some of the systests runnable in eclipse

--- systests/tomcat7/src/test/java/org/apache/cxf/fediz/integrationtests/ClientCertificateTest.java
+++ systests/tomcat7/src/test/java/org/apache/cxf/fediz/integrationtests/ClientCertificateTest.java
@@ -20,6 +20,8 @@
 package org.apache.cxf.fediz.integrationtests;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -29,6 +31,7 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleState;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.io.IOUtils;
 import org.apache.cxf.fediz.tomcat.FederationAuthenticator;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -107,6 +110,21 @@ public class ClientCertificateTest extends AbstractClientCertTests {
         } else {
             File rpWebapp = new File(baseDir + File.separator + server.getHost().getAppBase(), "simpleWebapp");
             Context cxt = server.addWebapp("/fedizhelloworld", rpWebapp.getAbsolutePath());
+            
+            // Substitute the IDP port. Necessary if running the test in eclipse where port filtering doesn't seem
+            // to work
+            File f = new File(currentDir + "/src/test/resources/fediz_config_client_cert.xml");
+            FileInputStream inputStream = new FileInputStream(f);
+            String content = IOUtils.toString(inputStream, "UTF-8");
+            inputStream.close();
+            if (content.contains("idp.https.port")) {
+                content = content.replaceAll("\\$\\{idp.https.port\\}", "" + idpHttpsPort);
+            
+                File f2 = new File(baseDir + "/test-classes/fediz_config_client_cert.xml");
+                FileOutputStream outputStream = new FileOutputStream(f2);
+                IOUtils.write(content, outputStream, "UTF-8");
+                outputStream.close();
+            }
             
             FederationAuthenticator fa = new FederationAuthenticator();
             fa.setConfigFile(currentDir + File.separator + "target" + File.separator
