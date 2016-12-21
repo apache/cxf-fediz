11c3bd821f353eccdbbf0c3f5fdbb1e8084642b7
Colm O hEigeartaigh <coheigea@apache.org>
2016-12-21 15:11:13 +0000

    Fixing backmerge

--- plugins/core/src/main/java/org/apache/cxf/fediz/core/processor/FederationProcessorImpl.java
+++ plugins/core/src/main/java/org/apache/cxf/fediz/core/processor/FederationProcessorImpl.java
@@ -519,24 +519,21 @@ public class FederationProcessorImpl extends AbstractFedizProcessor {
                 }
             }
             
+            if (logoutRedirectTo == null || logoutRedirectTo.isEmpty()) {
+                logoutRedirectTo = config.getLogoutRedirectTo();
+            }
+            
             if (logoutRedirectTo != null && !logoutRedirectTo.isEmpty()) {
+    
+                if (logoutRedirectTo.startsWith("/")) {
+                    logoutRedirectTo = extractFullContextPath(request).concat(logoutRedirectTo.substring(1));
+                } else {
+                    logoutRedirectTo = extractFullContextPath(request).concat(logoutRedirectTo);
+                }
+
                 LOG.debug("wreply={}", logoutRedirectTo);
                 sb.append('&').append(FederationConstants.PARAM_REPLY).append('=');
                 sb.append(URLEncoder.encode(logoutRedirectTo, "UTF-8"));
-            } else {
-                logoutRedirectTo = config.getLogoutRedirectTo();
-                if (logoutRedirectTo != null && !logoutRedirectTo.isEmpty()) {
-    
-                    if (logoutRedirectTo.startsWith("/")) {
-                        logoutRedirectTo = extractFullContextPath(request).concat(logoutRedirectTo.substring(1));
-                    } else {
-                        logoutRedirectTo = extractFullContextPath(request).concat(logoutRedirectTo);
-                    }
-    
-                    LOG.debug("wreply={}", logoutRedirectTo);
-                    sb.append('&').append(FederationConstants.PARAM_REPLY).append('=');
-                    sb.append(URLEncoder.encode(logoutRedirectTo, "UTF-8"));
-                }
             }
 
             redirectURL = redirectURL + "?" + sb.toString();
