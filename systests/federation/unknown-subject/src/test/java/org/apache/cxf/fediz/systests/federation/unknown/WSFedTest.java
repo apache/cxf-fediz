c3d34d5a3d4fb84b4a9bc0a560b9a19efcc3a64a
Colm O hEigeartaigh <coheigea@apache.org>
2017-07-12 18:03:30 +0100

    Removing some printlns

--- systests/federation/unknown-subject/src/test/java/org/apache/cxf/fediz/systests/federation/unknown/WSFedTest.java
+++ systests/federation/unknown-subject/src/test/java/org/apache/cxf/fediz/systests/federation/unknown/WSFedTest.java
@@ -191,7 +191,6 @@ public class WSFedTest {
         final String bodyTextContent =
             login(url, user, password, getIdpRealmbHttpsPort(), idpHttpsPort);
 
-        System.out.println("CONTENT: " + bodyTextContent);
         // Test for "realm b" principal here
         Assert.assertTrue("Principal not ALICE",
                           bodyTextContent.contains("userPrincipal=ALICE"));
@@ -251,8 +250,6 @@ public class WSFedTest {
         url2 += "wtrealm=" + URLEncoder.encode(wtrealm, "UTF8") + "&";
         url2 += "wresult=" + URLEncoder.encode(wresult, "UTF8");
         
-        System.out.println("RES: " + wresult);
-
         webClient2.getOptions().setJavaScriptEnabled(false);
         final HtmlPage idpPage2 = webClient2.getPage(url2);
         webClient2.getOptions().setJavaScriptEnabled(true);
