658498da70409172a5b7170ba8dd85a7fdf4d04e
gonzalad <adr_gonzalez@yahoo.fr>
2017-10-09 21:42:08 +0200

    FEDIZ-212: fix logout when no httpSession present
    When no httpSession is present, LogoutService
    throws a OAuthServiceException.
    
    This can be an issue if logout is called
    multiples times (i.e. from different OIDC Clients)

--- services/oidc/src/main/java/org/apache/cxf/fediz/service/oidc/logout/LogoutService.java
+++ services/oidc/src/main/java/org/apache/cxf/fediz/service/oidc/logout/LogoutService.java
@@ -71,18 +71,22 @@ public class LogoutService extends JoseJwtConsumer {
     protected Response doInitiateLogout(MultivaluedMap<String, String> params) {
 
         IdToken idTokenHint = getIdTokenHint(params);
-        OidcUserSubject subject = subjectCreator.createUserSubject(mc, params);
-
         Client client = getClient(params, idTokenHint);
-        if (backChannelLogoutHandler != null) {
-            backChannelLogoutHandler.handleLogout(client, subject, idTokenHint);
-        }
-        if (logoutHandlers != null) {
 
-            for (LogoutHandler handler : logoutHandlers) {
-                handler.handleLogout(client, subject);
+        if (mc.getSecurityContext().getUserPrincipal() != null) {
+            OidcUserSubject subject = subjectCreator.createUserSubject(mc, params);
+
+            if (backChannelLogoutHandler != null) {
+                backChannelLogoutHandler.handleLogout(client, subject, idTokenHint);
+            }
+            if (logoutHandlers != null) {
+
+                for (LogoutHandler handler : logoutHandlers) {
+                    handler.handleLogout(client, subject);
+                }
             }
         }
+
         // Clear OIDC session now
         mc.getHttpServletRequest().getSession().invalidate();
 
@@ -113,7 +117,7 @@ public class LogoutService extends JoseJwtConsumer {
         String clientLogoutUriParam = params.getFirst(CLIENT_LOGOUT_URI);
         if (uris.length > 1) {
             if (clientLogoutUriParam == null
-                || !new HashSet<>(Arrays.asList(uris)).contains(clientLogoutUriParam)) {
+                    || !new HashSet<>(Arrays.asList(uris)).contains(clientLogoutUriParam)) {
                 throw new BadRequestException();
             }
             uriStr = clientLogoutUriParam;
