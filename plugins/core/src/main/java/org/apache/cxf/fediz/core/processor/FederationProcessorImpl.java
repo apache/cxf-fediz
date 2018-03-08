8f745805ce4ca38f7a91d7ba343e705117314f18
Colm O hEigeartaigh <coheigea@apache.org>
2018-03-08 14:51:48 +0000

    Adding further DocType test for the plugins

--- plugins/core/src/main/java/org/apache/cxf/fediz/core/processor/FederationProcessorImpl.java
+++ plugins/core/src/main/java/org/apache/cxf/fediz/core/processor/FederationProcessorImpl.java
@@ -135,7 +135,7 @@ public class FederationProcessorImpl extends AbstractFedizProcessor {
         if ("RequestSecurityTokenResponseCollection".equals(el.getLocalName())) {
             el = DOMUtils.getFirstElement(el);
         }
-        if (!"RequestSecurityTokenResponse".equals(el.getLocalName())) {
+        if (el == null || !"RequestSecurityTokenResponse".equals(el.getLocalName())) {
             LOG.warn("Unexpected root element of wresult: '" + el.getLocalName() + "'");
             throw new ProcessingException(TYPE.INVALID_REQUEST);
         }
